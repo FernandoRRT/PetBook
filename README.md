4a3ed6fceff9a4c222e505dca1362b06d5e83274
Fernando <suporteiros@gmail.com>
2024-11-07 15:52:04 -0300

    chore: initial project and documentation
    - Initial project setup with Android Studio files
    - Created a README.md with the Project description and initial goals

---
+++ README.md
@@ -0,0 +1,17 @@
+# PetBook
+
+Projeto avaliativo da disciplina de persistência de dados do primeiro semestre da pós-graduação do IFSP
+
+## Descrição
+
+O projeto destina-se a criar uma planilha de registro de consultas médicas de um animal de estimação. Inicialmente, devido ao tempo disponível ele terá poucas funcionalidades, mas pretendo expandí-lo no futuro, como forma de praticar meus conhecimentos no desenvolvimento Android com Kotlin.
+
+### Comportamento inicial
+
+O que será persistido no banco:
+- id da consulta (chave primária)
+- nome do animal
+- nome da clínica onde foi feito o atendimento
+- o tipo de atendimento (Ex: Cirurgia, Vacinação, Consulta ou Exames)
+- data em que foi feita a consulta
+- descrição do atendimento (Ex: o dono fornece mais detalhes do atendimento, como o que foi receitado, etc)
---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "br.edu.ifsp.scl.sdm"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "br.edu.ifsp.scl.sdm"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    viewBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // room
+    implementation("androidx.room:room-runtime:2.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
+    ksp("androidx.room:room-compiler:2.6.0")
+
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+}
