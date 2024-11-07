ba26c68c54f341b317cf5b77a0a6967f9a9ea2e5
Fernando <suporteiros@gmail.com>
2024-11-07 16:37:25 -0300

    feat(layout): create basic layout for pet consultations app
    - Created the initial fragments to receive the appointment details
    - Architectural pattern MVVM

--- app/src/main/java/br/edu/ifsp/scl/sdm/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package br.edu.ifsp.scl.sdm
+package br.edu.ifsp.scl.sdm.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sdm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
